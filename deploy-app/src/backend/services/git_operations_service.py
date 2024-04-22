8414d5a3bf155f72c8d2ea0b2f651b1eda479095
Paul P <bigpaulie25ro@yahoo.com>
2024-04-22 12:31:59 +0300

    feat: better error messages for precedence violation

--- deploy-app/src/backend/services/git_operations_service.py
+++ deploy-app/src/backend/services/git_operations_service.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 
 from backend.models import TagData
 from backend.services.git_service import GitService  # Adjust the import path as necessary
-from backend.constants import RELEASE_FILE_NAME, RELEASE_TEMPLATE_FILE_NAME, TEMPLATES_DIR, VERSIONS_FILE_NAME
+from backend.constants import RELEASE_FILE_NAME, RELEASE_TEMPLATE_FILE_NAME, VERSIONS_FILE_NAME
 from backend.exceptions.git_operation_exception import GitOperationException
 
 def represent_ordereddict(dumper, data):
@@ -26,21 +26,23 @@ class GitOperationsService:
         self.config_file_path = config_file_path
 
     def update_and_push_changes(self, tag_data: TagData, user=None):
-        try:
-            self.prepare_repository()
-            if not self.check_tag_precedence(tag_data):
-                raise ValueError(f"The provided tag does not respect the precedence rule.")
-            self.update_configuration_file(tag_data)
-            commiting_user = user['name']
-            commit_message = f'chore: {tag_data.fromVersion} to {tag_data.toVersion} {commiting_user} for {tag_data.directory} {tag_data.environment}'
-            self.git_service.git_commit(repo_path=self.local_repo_path, message=commit_message)
-            self.git_service.git_push(self.local_repo_path)
-        except GitOperationException as e:
-            logger.error(f"Git operation failed: {e}")
-            raise
-        except Exception as e:
-            logger.error(f"Failed to update and push changes: {str(e)}")
-            raise
+      try:
+          self.prepare_repository()
+          precedence_check, violation_info = self.check_tag_precedence(tag_data)
+          if not precedence_check:
+              raise ValueError(f"Evironment precedence violation: Attempting to set version '{tag_data.tag}' for '{tag_data.environment}' exceeds version '{violation_info['current_version']}' set in '{violation_info['env']}' environment.")
+          self.update_configuration_file(tag_data)
+          committing_user = user['name'] if user else 'Unknown User'
+          commit_message = f'chore: {tag_data.fromVersion} to {tag_data.toVersion} {committing_user} for {tag_data.directory} {tag_data.environment}'
+          self.git_service.git_commit(repo_path=self.local_repo_path, message=commit_message)
+          self.git_service.git_push(self.local_repo_path)
+      except GitOperationException as e:
+          logger.error(f"Git operation failed: {e}")
+          raise
+      except Exception as e:
+          logger.error(f"Failed to update and push changes: {str(e)}")
+          raise
+
 
     def prepare_repository(self):
         try:
@@ -57,19 +59,7 @@ class GitOperationsService:
         try:
             with open(config_file, 'r') as file:
                 config = yaml.load(file, Loader=yaml.FullLoader)
-                env_order = [env['name'] for env in config['application']['envs']]
-
-                # # Create an OrderedDict to preserve the order
-                # ordered_envs = OrderedDict()
-                # for env in env_order:
-                #     ordered_envs[env] = config['application']['envs'].get(env, '')
-                # print(f"Ordered envs: {ordered_envs}")
-
-                # # Update the specific environment value
-                # ordered_envs[tag_data.environment] = tag_data.tag
-
-                # # Update the config dictionary with the ordered environments
-                # config['application']['envs'] = ordered_envs
+                
                 for env in config['application']['envs']:
                     if env['name'] == tag_data.environment:
                         env['version'] = tag_data.tag
@@ -82,37 +72,44 @@ class GitOperationsService:
             raise
 
     def check_tag_precedence(self, tag_data):
-        print(f"Checking tag precedence for {tag_data.tag} in {tag_data.environment}, {self.config_file_path}")
-        config_file = os.path.join(self.local_repo_path, self.config_file_path)
-        with open(config_file, 'r') as file:
-            config = yaml.safe_load(file)
-        
-        env_order = [env['name'] for env in config['application']['envs']]
+      print(f"Checking tag precedence for {tag_data.tag} in {tag_data.environment}, {self.config_file_path}")
+      config_file = os.path.join(self.local_repo_path, self.config_file_path)
+      with open(config_file, 'r') as file:
+          config = yaml.safe_load(file)
 
-        proposed_version = StrictVersion(tag_data.tag)
-        for env in env_order:
-            if env == tag_data.environment:
-                break  # No need to check versions for the same or lower environments
+      # Get the order of the environments from the versions file
+      precedence = []
+      versions_file = os.path.join(self.local_repo_path, tag_data.directory, VERSIONS_FILE_NAME)
+      with open(versions_file, 'r') as file:
+          versions = yaml.safe_load(file)
+          precedence = versions['application'].get('envs', [])
+      
+      proposed_version = StrictVersion(tag_data.tag)
+      violation_info = {}
+      for env in precedence:
+          if env == tag_data.environment:
+              break  # No need to check versions for the same or lower environments
 
-            # find the current version for the environment in the config file applications.envs[].version
-            # the envs is an list of objects with name and version keys
-            current_version_str = None
-            for env_obj in config['application']['envs']:
-                if env_obj['name'] == env:
-                    current_version_str = env_obj['version']
-                    break
+          # Find the current version for the environment in the config file
+          current_version_str = None
+          for env_obj in config['application']['envs']:
+              if env_obj['name'] == env:
+                  current_version_str = env_obj['version']
+                  break
 
-            if current_version_str and StrictVersion(current_version_str) < proposed_version:
-                return False  # Precedence violated
-        
-        return True  # Precedence maintained
+          if current_version_str and StrictVersion(current_version_str) < proposed_version:
+              violation_info = {'env': env, 'current_version': current_version_str, 'proposed_version': proposed_version}
+              return False, violation_info  # Precedence violated
+          
+      return True, None  # Precedence maintained
+
     
     def find_release_files(self):
         directories_with_release = []
         for root, dirs, files in os.walk(self.local_repo_path, topdown=True):
             dirs[:] = [d for d in dirs if not d.startswith('.')]  # Ignore hidden directories
-            if 'release.yaml' in files:
-                release_file_path = os.path.join(root, 'release.yaml')
+            if RELEASE_FILE_NAME in files:
+                release_file_path = os.path.join(root, RELEASE_FILE_NAME)
                 with open(release_file_path, 'r') as file:
                     release_data = yaml.safe_load(file)
                 app_info = {
@@ -128,7 +125,7 @@ class GitOperationsService:
         Creates a default release.yaml file for the specified directory.
         This method will use a template to create the file.
         """
-        template_file_path = os.path.join(self.local_repo_path, TEMPLATES_DIR, RELEASE_TEMPLATE_FILE_NAME)
+        template_file_path = os.path.join(self.local_repo_path, directory_name, RELEASE_TEMPLATE_FILE_NAME)
         release_file_path = os.path.join(self.local_repo_path, directory_name, RELEASE_FILE_NAME)
 
         try:
