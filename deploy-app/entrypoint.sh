603bba2a6c7a842c7e05bc936f2b265c3df45da3
Andrei Damian <andrei@lummetry.ai>
2024-04-26 16:15:34 +0300

    fix: version

--- deploy-app/entrypoint.sh
+++ deploy-app/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+APP_VER=$(cat ver.txt)
+
 # Replace placeholders with environment variable values in JavaScript files
 sed -i "s|__REACT_APP_API_URL__|${REACT_APP_API_URL}|g" /app/frontend/dist/assets/*.js
+
+sed -i "s|__REACT_APP_VER__|${APP_VER}|g" /app/frontend/dist/assets/*.js
 
 # Execute the passed command
 exec "$@"
--- deploy-app/src/frontend/src/Constants.ts
+++ deploy-app/src/frontend/src/Constants.ts
@@ -1,2 +1,2 @@
 export const BASE_API_URL = "http://localhost:8000";
-export const APP_NAME = "Hyfy Deploy";
+export const APP_NAME = "Hyperfy Release Manager v__REACT_APP_VER__";
